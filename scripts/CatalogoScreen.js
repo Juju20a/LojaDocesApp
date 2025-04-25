5eb16cb8b3d3e40b8403709c9836f36367dbc963
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2025-04-25 09:53:34 -0300

    CatalogoScreen.js

--- scripts/CatalogoScreen.js
+++ scripts/CatalogoScreen.js
@@ -5,9 +5,9 @@ import {
 } from 'react-native';
 
 const docesIniciais = [
-  { id: '1', nome: 'Brigadeiro', preco: 2.5, categoria: 'Chocolate', imagem: 'https://i.imgur.com/M1XzKzM.jpg' },
-  { id: '2', nome: 'Beijinho', preco: 2.0, categoria: 'Coco', imagem: 'https://i.imgur.com/vVjQdAq.jpg' },
-  { id: '3', nome: 'Trufa', preco: 3.5, categoria: 'Recheado', imagem: 'https://i.imgur.com/efy5E0v.jpg' },
+  { id: '1', nome: 'Brigadeiro', preco: 1.5, categoria: 'Chocolate', imagem: 'https://images.unsplash.com/photo-1702982852429-e0d0b27eb990?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QnJpZ2FkZWlyb3xlbnwwfHwwfHx8MA%3D%3D' },
+  { id: '2', nome: 'Beijinho', preco: 1.5, categoria: 'Coco', imagem: 'https://media.istockphoto.com/id/1273771672/pt/foto/homemade-brazilian-sweets-beijinho-and-bicho-de-p%C3%A9-are-traditional-brazilian-dessert-orange.webp?a=1&b=1&s=612x612&w=0&k=20&c=By74yAnJTMloPispTYDtsv3bAQ1jEGmxPxIeU5MSMPo=' },
+  { id: '3', nome: 'Trufa', preco: 3.5, categoria: 'Recheado', imagem: 'https://media.istockphoto.com/id/1487195471/pt/foto/chocolate-candies-collection-beautiful-belgian-truffles-isolated-on-white-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=Beo7HqGnh850yX3iC3BfwZTR43izSIogdhMiiYSU-38=' },
 ];
 
 export default function CatalogoScreen() {
