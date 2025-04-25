37e94807bdf204d078966037a3a15913becbb272
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2025-04-25 09:39:18 -0300

    layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,8 @@ export default function Layout() {
       <Tabs.Screen name="index" options={{ title: 'Início' }} />
       <Tabs.Screen name="loja-doces" options={{ title: 'Loja de Doces' }} />
       <Tabs.Screen name="cadastro-cliente" options={{ title: 'Cadastro-Clientes' }} />
+      <Tab.Screen name="Catálogo" component={CatalogoScreen} />
+
 
     </Tabs>
   );
